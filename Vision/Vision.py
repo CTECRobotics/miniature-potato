fce5e320a478c259ae4f7e156c2e25e7de4a5171
Joey360 <Joey360@users.noreply.github.com>
2018-01-07 14:50:47 -0800

    Add files via upload

---
+++ Vision/Vision.py
@@ -0,0 +1,6 @@
+from blue import blue
+from red import red
+import pyfrc
+from networktables import NetworkTables
+NetworkTables.initialize(server='roborio-6445-frc.local')
+sd = NetworkTables.getTable('SmartDashboard')
