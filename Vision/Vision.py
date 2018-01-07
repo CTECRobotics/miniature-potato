602b8d43e8d18cd7747ddbb910697893edd50763
team6445 <team6445@gmail.com>
2018-01-07 15:30:51 -0800

    Logic setup

--- Vision/Vision.py
+++ Vision/Vision.py
@@ -1,8 +1,19 @@
 from blue import blue
 from red import red
-import pyfrc
+import cv2
 from networktables import NetworkTables
 NetworkTables.initialize(server='roborio-6445-frc.local')
 sd = NetworkTables.getTable('SmartDashboard')
-jetson = NetworkTables.getTable('jetson')
+Jetson = NetworkTables.getTable('Jetson')
 while True:
+    color = sd.getvalue("Alliance_Color")
+    position = sd.getvalue("Auto_Mode")
+    if "blue" in color:
+        blue.process(blue, cv2.VideoCapture(0))
+    if "red" in color:
+        red.process(red, cv2.VideoCapture(0))
+    if position is 1:
+
+    if position is 2:
+
+    if position is 3:
