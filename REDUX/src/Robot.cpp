0bd9e05f6dd743a504cd08b63c910489f202dcf5
Angel Huang <GoldArchex@gmail.com>
2018-01-13 13:58:14 -0800

    Add files via upload

--- REDUX/src/Robot.cpp
+++ REDUX/src/Robot.cpp
@@ -29,31 +29,25 @@ public:
 		//CONTROL VALUES
 	double CAMERA_ERROR;	//POSTIVE IS NEGATIVE, RIGHT IS POSITIVE
 	double SETPOINT_GYRO;
-	double NAVX_GRYO;
-	double ADX_GRYO;
-	double COMPOSITE_GYRO;
-	double CURRENT_SETPOINT;
-	double L_PRM;
-	double R_RPM;
-	double L_VELOCITY;
+	double NAVX_GRYO;		//OUTPUT FROM THE NAVX
+	double ADX_GRYO;		//OUTPUT FROM THE SPI
+	double COMPOSITE_GYRO;	//AVERAGED VALUE OF GYROSCOPES
+	double L_VELOCITY;		//VELOCITY OF THE ENCODERS IN TICKS
 	double R_VELOCITY;
 	double ELEV_POSITION;
-	double WINCH_POSITION;
-	double DISTANCE;
-	double RANGE_METERS;
-	double OUTPUT;
 	double THROTTLE;
 	double STEER;
+	double UNIV_A_SET;		//UNIVERSAL
 	std::string ALLIANCE;
 	std::string ALLIED_POS;
 		//DON'T TOUCH ME VALUES
 	double MAXIMUM_RPM;
 	double VALVE_WAIT;
-	double ARRAY_WP_1 [3];
-	double ARRAY_WP_2 [3];
-	double ARRAY_WP_3 [3];
-	const int ELEV_SCALE;
-	const int WINCH_SCALE;
+	double ARRAY_WP_1 [2]= {5, 90};
+	double ARRAY_WP_2 [2]= {5, 90};
+	double ARRAY_WP_3 [2]= {5, 90};
+	const int ELEV_SCALE = 1;
+	const int WINCH_SCALE = 1;
 		//MISC
 
 
@@ -74,6 +68,8 @@ public:
 	DoubleSolenoid *GEARBOX_L;
 	DoubleSolenoid *GEARBOX_R;
 	DoubleSolenoid *ELEV_WINCH;
+	DoubleSolenoid *ARM_L;
+	DoubleSolenoid *ARM_R;
 		//SENSOR INPUTS
 	Encoder *MOTOR_ENCODER_L;
 	Encoder *MOTOR_ENCODER_R;
@@ -95,27 +91,20 @@ public:
 		VALVE_R = 1;
 		AUTO_MODE = 0;
 			//CONTROL VALUES
-		CAMERA_ERROR = 0;
 		SETPOINT_GYRO = 0;
 		NAVX_GRYO = 0;
 		ADX_GRYO = 0;
 		COMPOSITE_GYRO = 0;
-		CURRENT_SETPOINT = 0;
 		L_VELOCITY = 0;
 		R_VELOCITY = 0;
-		DISTANCE = 0;
-		OUTPUT = 0;
 		THROTTLE = 0;
 		STEER = 0;
-		ALLIANCE = "";
-		ALLIED_POS = "";
+		UNIV_A_SET = 0;		//THIS IS THE UNIVERSAL ANGLE SETPOINT FOR AUTO
+//		ALLIANCE = "";
+//		ALLIED_POS = "";
 			//DO NOT MODIFY
 		MAXIMUM_RPM = 511;
 		VALVE_WAIT = 0.25;
-			//ARRAYS
-		ARRAY_WP_1 [2]= {5, 90};
-		ARRAY_WP_2 [2]= {5, 90};
-		ARRAY_WP_3 [2]= {5, 90};
 
 				//PERIPHERAL AND CONTROL SETUP
 			//TALONSRX
@@ -129,28 +118,10 @@ public:
 		GEARBOX_L = new DoubleSolenoid(1, 1, 1);
 		GEARBOX_R = new DoubleSolenoid(1, 1, 1);
 		ELEV_WINCH = new DoubleSolenoid(1, 1, 1);
-			//ENCODERS
-		MOTOR_ENCODER_L = new Encoder(0, 1, false, Encoder::EncodingType::k4X);
-		MOTOR_ENCODER_L->SetMaxPeriod(.1);
-		MOTOR_ENCODER_L->SetMinRate(10);
-		MOTOR_ENCODER_L->SetDistancePerPulse(5);
-		MOTOR_ENCODER_L->SetReverseDirection(false);
-		MOTOR_ENCODER_L->SetSamplesToAverage(7);
+		ARM_L = new DoubleSolenoid(1, 1, 1);
+		ARM_R = new DoubleSolenoid(1, 1, 1);
 
-		MOTOR_ENCODER_R = new Encoder(2, 3, false, Encoder::EncodingType::k4X);
-		MOTOR_ENCODER_R->SetMaxPeriod(.1);
-		MOTOR_ENCODER_R->SetMinRate(10);
-		MOTOR_ENCODER_R->SetDistancePerPulse(5);
-		MOTOR_ENCODER_R->SetReverseDirection(false);
-		MOTOR_ENCODER_R->SetSamplesToAverage(7);
-
-		WINCH_ELEV = new Encoder(2, 3, false, Encoder::EncodingType::k4X);
-		WINCH_ELEV->SetMaxPeriod(.1);
-		WINCH_ELEV->SetMinRate(10);
-		WINCH_ELEV->SetDistancePerPulse(5);
-		WINCH_ELEV->SetReverseDirection(false);
-		WINCH_ELEV->SetSamplesToAverage(7);
-			//NAVX CONNECTION ATTEMPT
+//			//NAVX CONNECTION ATTEMPT
         try {
             PHYSIX = new AHRS(SPI::Port::kMXP);
         } catch (std::exception ex ) {
@@ -165,11 +136,11 @@ public:
 		AUTO_TIMER = new Timer();
 		INTERNAL_ACCELEROMETER = new BuiltInAccelerometer();
 			//INITIAL SETPOINTS, CALIB, ETC.
+				//ControlMode::(mode) IS NOW USED TO DETERMINE CONTROL METHOD
 		MOTOR_LM->Set(ControlMode::PercentOutput, 0);
 		MOTOR_RM->Set(ControlMode::PercentOutput, 0);
 		MOTOR_LS->Set(ControlMode::PercentOutput, 0);
 		MOTOR_RS->Set(ControlMode::PercentOutput, 0);
-		CONTROL_GYRO->Calibrate();
 		MOTOR_LS->Set(ControlMode::Follower, 1);
 		MOTOR_RS->Set(ControlMode::Follower, 3);
 		WINCH_ELEV_1->Set(ControlMode::PercentOutput, 0);
@@ -177,19 +148,108 @@ public:
 		WINCH_ELEV_2->Set(ControlMode::Follower, 5);
 		ACTUATOR_1->Set(ControlMode::PercentOutput, 0);
 		ACTUATOR_2->Set(ControlMode::PercentOutput, 0);
+		CONTROL_GYRO->Calibrate();
 			//MISC
-		//MOTOR_L_PID = new PIDController(1, 1, 1, MOTOR_ENCODER_L, MOTOR_LM);
-		//MOTOR_R_PID = new PIDController(1, 1, 1, MOTOR_ENCODER_R, MOTOR_RM);
+			//INTERNAL PID SETUP
+		MOTOR_LM->GetSelectedSensorVelocity(1);
+				//SELECTS SENSOR BASED OFF CHANNEL
+		MOTOR_LM->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, 10);
+				//SETS UP SENSOR TYPE, PID SLOT (DEFINED AS pidIdx) NUMBER, TIMEOUT
+		MOTOR_LM->SetSensorPhase(true);
+				//DETERMINES READ DIRECTIONS
+
+		MOTOR_LM->ConfigNominalOutputForward(0, 10);
+		MOTOR_LM->ConfigNominalOutputReverse(0, 10);
+		MOTOR_LM->ConfigPeakOutputForward(1, 10);
+		MOTOR_LM->ConfigPeakOutputReverse(-1, 10);
+
+				//SETS UP THE VARIOUS VALUES IN THE PID SLOT
+		MOTOR_LM->Config_kF(0, 0.0, 10);
+		MOTOR_LM->Config_kP(0, 0.1, 10);
+		MOTOR_LM->Config_kI(0, 0.0, 10);
+		MOTOR_LM->Config_kD(0, 0.0, 10);
+
+		MOTOR_RM->GetSelectedSensorVelocity(2);
+		MOTOR_RM->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, 10);
+		MOTOR_RM->SetSensorPhase(true);
+
+		MOTOR_RM->ConfigNominalOutputForward(0, 10);
+		MOTOR_RM->ConfigNominalOutputReverse(0, 10);
+		MOTOR_RM->ConfigPeakOutputForward(1, 10);
+		MOTOR_RM->ConfigPeakOutputReverse(-1, 10);
+
+		MOTOR_RM->Config_kF(0, 0.0, 10);
+		MOTOR_RM->Config_kP(0, 0.1, 10);
+		MOTOR_RM->Config_kI(0, 0.0, 10);
+		MOTOR_RM->Config_kD(0, 0.0, 10);
+
 		VISION_DATA = nt::NetworkTable::GetTable("JETSON");
 
 	}
-	int ELEV_POSITION(int DEGREES) {
-		double ELEV_POSITION = DEGREES*ELEV_SCALE;
+	void TURN_TO_ANGLE (int UNIV_A_SET ) {
+		if(COMPOSITE_GYRO < UNIV_A_SET ){
+			MOTOR_LM->Set(ControlMode::Velocity, -0.5);
+			MOTOR_RM->Set(ControlMode::Velocity, 0.5);
+		} else if(COMPOSITE_GYRO > UNIV_A_SET ) {
+
+		} else {
+
+		}
+
+	}
+	void DRIVE_TO_DISTANCE() {
+		if(CONTROL_GYRO->GetAngle() <= (UNIV_A_SET - 2)) {
+			MOTOR_LM->Set(ControlMode::PercentOutput, 0.15);
+			MOTOR_RM->Set(ControlMode::PercentOutput, -0.08);
+		} else if(CONTROL_GYRO->GetAngle() >= (UNIV_A_SET + 2)) {
+			MOTOR_LM->Set(ControlMode::PercentOutput, 0.05);
+			MOTOR_RM->Set(ControlMode::PercentOutput, -0.18);
+		} else if(CONTROL_GYRO->GetAngle() > (UNIV_A_SET - 2) || CONTROL_GYRO->GetAngle() < (UNIV_A_SET + 2)) {
+			MOTOR_LM->Set(ControlMode::PercentOutput, 0.25);
+			MOTOR_RM->Set(ControlMode::PercentOutput, -0.28);
+		}
+	}
+	int CALC_ELEV_POSITION(int ELEVATOR_DEGREES) {
+		double ELEV_POSITION = ELEVATOR_DEGREES*ELEV_SCALE;
 		return ELEV_POSITION;
 	}
-	int WINCH_POSITION() {
-		double WINCH_POSITION = DEGREES*WINCH_SCALE;
-		return WINCH_POSITION;
+	void SHIFT_HIGH () {
+		GEARBOX_L->Set(DoubleSolenoid::kForward);
+		GEARBOX_R->Set(DoubleSolenoid::kForward);
+		frc::Wait(0.5);
+		GEARBOX_L->Set(DoubleSolenoid::kOff);
+		GEARBOX_R->Set(DoubleSolenoid::kOff);
+	}
+	void SHIFT_LOW () {
+		GEARBOX_L->Set(DoubleSolenoid::kReverse);
+		GEARBOX_R->Set(DoubleSolenoid::kReverse);
+		frc::Wait(0.5);
+		GEARBOX_L->Set(DoubleSolenoid::kOff);
+		GEARBOX_R->Set(DoubleSolenoid::kOff);
+	}
+	void ACTUATOR_UP() {
+		ARM_L->Set(DoubleSolenoid::kReverse);
+		ARM_R->Set(DoubleSolenoid::kReverse);
+		frc::Wait(0.5);
+		ARM_L->Set(DoubleSolenoid::kOff);
+		ARM_R->Set(DoubleSolenoid::kOff);
+	}
+	void ACTUATOR_DOWN () {
+		ARM_L->Set(DoubleSolenoid::kForward);
+		ARM_R->Set(DoubleSolenoid::kForward);
+		frc::Wait(0.5);
+		ARM_L->Set(DoubleSolenoid::kOff);
+		ARM_R->Set(DoubleSolenoid::kOff);
+	}
+	void CHANGE_TO_WINCH () {
+		ELEV_WINCH->Set(DoubleSolenoid::kReverse);
+		frc::Wait(0.5);
+		ELEV_WINCH->Set(DoubleSolenoid::kOff);
+	}
+	void CHANGE_TO_ELEVATOR () {
+		ELEV_WINCH->Set(DoubleSolenoid::kForward);
+		frc::Wait(0.5);
+		ELEV_WINCH->Set(DoubleSolenoid::kOff);
 	}
 	void AutonomousInit() override {
 		ALLIANCE = std::to_string(DS->GetInstance().GetAlliance());
@@ -204,7 +264,6 @@ public:
 		AUTO_TIMER->Reset();
 		AUTO_TIMER->Start();
 		CONTROL_GYRO->Reset();
-		SETPOINT_GYRO = CONTROL_GYRO->GetAngle();
 	}
 
 	void AutonomousPeriodic() {
@@ -277,22 +336,34 @@ public:
 
 		MOTOR_LM->Set(ControlMode::PercentOutput, THROTTLE+STEER);
 		MOTOR_RM->Set(ControlMode::PercentOutput, -THROTTLE+STEER);
+		if (LEFT_JOYSTICK->GetRawButton(1)) {
+			SHIFT_HIGH();
+		}
+		if (LEFT_JOYSTICK->GetRawButton(2)) {
+			SHIFT_LOW();
+		}
+		if (LEFT_JOYSTICK->GetRawButton(11)) {
+			ACTUATOR_UP();
+		}
+		if (LEFT_JOYSTICK->GetRawButton(8)) {
+			ACTUATOR_DOWN();
+		}
+
 
 		NAVX_GRYO = PHYSIX->GetAngle();
 		ADX_GRYO = CONTROL_GYRO->GetAngle();
 		COMPOSITE_GYRO = ((NAVX_GRYO + ADX_GRYO)/2);
 
-		L_VELOCITY = MOTOR_ENCODER_L->GetRate(); //*SOME SCALE FACTOR
-		R_VELOCITY = MOTOR_ENCODER_R->GetRate();
+		L_VELOCITY = MOTOR_LM->GetSelectedSensorVelocity(1); //*SOME SCALE FACTOR
+		R_VELOCITY = MOTOR_RM->GetSelectedSensorVelocity(3);
 
-		//-OUTPUT = VOLTAGE_CONVERT(AUTO_RANGEFINDER->GetVoltage());
+//		OUTPUT = VOLTAGE_CONVERT(AUTO_RANGEFINDER->GetVoltage());
 		SmartDashboard::PutNumber("RPMS_L",  MOTOR_ENCODER_L->GetRate());
 		SmartDashboard::PutNumber("RPMS_R",  MOTOR_ENCODER_R->GetRate());
 		SmartDashboard::PutNumber("ADX_OUTPUT", CONTROL_GYRO->GetAngle());
 		SmartDashboard::PutNumber("NAVX_OUTPUT", PHYSIX->GetAngle());
 		SmartDashboard::PutNumber("COMPOSITE", COMPOSITE_GYRO);
 		SmartDashboard::PutNumber("RANGE", AUTO_RANGEFINDER->GetVoltage());
-		SmartDashboard::PutNumber("RANGE_METERS", OUTPUT);
 		SmartDashboard::PutNumber("X", INTERNAL_ACCELEROMETER->GetX());
 		SmartDashboard::PutNumber("Y", INTERNAL_ACCELEROMETER->GetY());
 		SmartDashboard::PutNumber("Z", INTERNAL_ACCELEROMETER->GetZ());
